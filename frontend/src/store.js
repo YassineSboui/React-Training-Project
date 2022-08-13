a6ae352d87a8cae922d66bc9d3435ee3c59bda1c
Yassine Sboui <ys.sboui7@gmail.com>
2022-08-14 00:08:02 +0100

    Redux

---
+++ frontend/src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+const initialState = {}
+const middleware = [thunk]
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
