399d7f639d4d1a7fc16aa7bc4cc65d996e65f88c
Yassine Sboui <ys.sboui7@gmail.com>
2022-08-17 02:36:17 +0100

    1. Qty Select & Add To Cart Button

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { productListReducer } from './reducers/productReducers.js'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js'
 import { batchedSubscribe } from 'redux-batched-subscribe'
 import _debounce from 'lodash/debounce'
-import logger from 'redux-logger'
 
 const reducer = {
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 }
 const initialState = {}
 const debounceNotify = _debounce((notify) => notify())
 
 const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
   devTools: process.env.NODE_ENV !== 'production',
   initialState,
   enhancers: [batchedSubscribe(debounceNotify)],
