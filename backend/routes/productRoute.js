baadc3cef6a6498ce9045a310151d2336035c613
Yassine Sboui <ys.sboui7@gmail.com>
2022-08-29 11:11:22 +0100

    User auth

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,33 +1,12 @@
 import express from 'express'
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
 
-//@desc Fetch all products
-//@route GET /api/products
-//@acess Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
-//@desc Fetch Single product
-//@route GET /api/products/:id
-//@acess Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
+router.route('/').get(getProducts)
 
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
